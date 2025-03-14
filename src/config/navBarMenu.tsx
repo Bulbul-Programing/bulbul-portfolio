96ebe7db7a364d2cfe2a2e4531e5e5de3a825ac9
Bulbul Ahammed Riad <bulbulprograming@gmail.com>
2025-03-15 00:11:32 +0600

    project section done

--- src/config/navBarMenu.tsx
+++ src/config/navBarMenu.tsx
@@ -1,6 +1,7 @@
 import { FaRegUser } from "react-icons/fa";
 import { IoNewspaperOutline } from "react-icons/io5";
 import { FaLaptopCode } from "react-icons/fa";
+import { FaPaperPlane } from "react-icons/fa";
 
 export const navbarItem = [
   {
@@ -12,6 +13,11 @@ export const navbarItem = [
     path: "/resume",
     element: "Resume",
     icon: <IoNewspaperOutline size={20} />,
+  },
+  {
+    path: "/contact",
+    element: "Contact",
+    icon: <FaPaperPlane size={20} />,
   },
   {
     path: "/project",
--- src/utils/ProjectData.ts
+++ src/utils/ProjectData.ts
@@ -1,8 +1,47 @@
 export const projectData = [
   {
-    title: "My Portfolio",
-    description: "A showcase of my web development projects",
-    link: "https://bulbulahammed.vercel.app",
-    image: ["https://res.cloudinary.com/depy0i4bl/image/upload/v1741878028/Screenshot_2025-03-05_220836_acdsqr.jpg", ],
+    title: "Taste tribe",
+    description: "The Recipe Sharing Community is a full-stack web application designed for cooking enthusiasts to share, discover, and organize recipes. Users can create and update their own recipes, follow other users, and engage with the community. The platform provides an interactive experience with user profiles, recipe ratings, and a seamless browsing experience.",
+    link: "https://taste-tribe-clint.vercel.app",
+    image: [
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878028/Screenshot_2025-03-05_220836_acdsqr.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741969792/Screenshot_2025-03-14_222843_qg6uni.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741969793/Screenshot_2025-03-14_222916_fhl2iq.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878025/Screenshot_2025-03-05_220923_qgg6nd.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878026/Screenshot_2025-03-05_221033_ijjfox.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878023/Screenshot_2025-03-05_221105_lt01tp.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878024/Screenshot_2025-03-05_221153_axrs86.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878026/Screenshot_2025-03-05_221248_z5gg5o.jpg"
+    ],
   },
+  {
+    title: "Fiber IT Institute",
+    description: "The IT Institute Website is a full-stack web application designed to provide an online platform for students to explore and enroll in IT courses. The platform features a structured course catalog, a blog section for tech-related articles, and an instructor application system where users can submit their CVs to become instructors.",
+    link: "https://fiber-it-institute-frontend.vercel.app",
+    image: [
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878027/Screenshot_2025-03-05_221737_s6kaus.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878028/Screenshot_2025-03-05_221804_mfzkdk.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878026/Screenshot_2025-03-05_221833_qexpg6.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878026/Screenshot_2025-03-05_221903_cgfe4z.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878027/Screenshot_2025-03-05_221926_mmed9x.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741878023/Screenshot_2025-03-05_222658_luaqkk.jpg"
+    ]
+  },
+  {
+    title: "Field Time",
+    description: "The IT Institute Website is a full-stack web application designed to provide an online platform for students to explore and enroll in IT courses. The platform features a structured course catalog, a blog section for tech-related articles, and an instructor application system where users can submit their CVs to become instructors.",
+    link: "https://fiber-it-institute-frontend.vercel.app",
+    image: [
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975078/Screenshot_2025-03-14_234114_i95hoa.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975079/Screenshot_2025-03-14_234203_ic8xff.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975078/Screenshot_2025-03-14_234312_l6djqe.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975077/Screenshot_2025-03-14_234424_li18xz.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975088/screencapture-assignment-five-three-vercel-app-facility-2025-03-14-23_50_56_lfkeee.png",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975076/Screenshot_2025-03-14_235144_ibycpg.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975076/Screenshot_2025-03-14_235327_b2w2fy.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975076/Screenshot_2025-03-14_235409_iilu0d.jpg",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975076/screencapture-assignment-five-three-vercel-app-user-dashboard-home-2025-03-14-23_55_25_c5gmj5.png",
+      "https://res.cloudinary.com/depy0i4bl/image/upload/v1741975076/Screenshot_2025-03-14_235632_mvtufu.jpg"
+    ]
+  }
 ];
